898f147e72b260a8014d0c002af5ac1d56a7c1a1
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-06-29 18:46:51 +1000

    update

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -72,8 +72,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             <div id="amount-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                   Please enter a valid amount greater than $0.
@@ -122,8 +122,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
               <div id="customer-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.customerId &&
-                  state.errors.customerId.map((error: string) => (
+                {state.errors?.status &&
+                  state.errors.status.map((error: string) => (
                     <p className="mt-2 text-sm text-red-500" key={error}>
                       {error}
                       Please select a status.
