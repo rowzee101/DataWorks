797f40b3a01261dad57cc2ddb8ae267ca167d518
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-06-29 18:38:19 +1000

    Update create-form.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -67,9 +67,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error" 
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                  Please enter a valid amount greater than $0.
+                </p>
+              ))}
+          </div>
           </div>
         </div>
 
@@ -87,6 +97,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -109,6 +120,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 >
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
+              </div>
+              <div id="customer-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.customerId &&
+                  state.errors.customerId.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                      Please select a status.
+                    </p>
+                  ))}
               </div>
             </div>
           </div>
