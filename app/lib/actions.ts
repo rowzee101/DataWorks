9553050a8980e49c4ae2a18ea1f9f000b98a5080
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-06-29 17:19:43 +1000

    creating invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';  // update the cashe i.e. the invoices currently displayed
+import { redirect } from 'next/navigation'; // redirect to the invoices page after creating an invoice
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices'); // revalidate the path to update the invoices displayed
+  redirect('/dashboard/invoices'); // redirect to the invoices page after creating an invoice
+}
