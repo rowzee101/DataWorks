0a447697475e635026b958285f43c711708be4b9
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-07-02 20:32:59 +1000

    Revert "aria"
    This reverts commit 4b65e837ee2012d770e90b38215a9c782639c03f.

--- app/dashboard/trials/page.tsx
+++ app/dashboard/trials/page.tsx
@@ -1,17 +1,3 @@
-import {Button, Dialog, DialogTrigger, Heading, Input, Label, Modal, TextField} from 'react-aria-components';
-
-
 export default function Page() {
-  return <p>
-    <DialogTrigger>
-      <Button>Open dialog</Button>
-      <Modal isKeyboardDismissDisabled>
-        <Dialog>
-          <Heading slot="title">Notice</Heading>
-          <p>You must close this dialog using the button below.</p>
-          <Button slot="close">Close</Button>
-        </Dialog>
-      </Modal>
-    </DialogTrigger>
-  </p>;
+  return <p>Trials Page</p>;
 }
