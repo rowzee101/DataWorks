d384797a7d3fc37f8f64f31bc0a59bca6a41f185
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-07-02 20:05:57 +1000

    aria

--- app/dashboard/trials/page.tsx
+++ app/dashboard/trials/page.tsx
@@ -1,3 +1,22 @@
+
+import {Button, Dialog, DialogTrigger, Heading, Input, Label, Modal, TextField} from 'react-aria-components';
+
+
+
 export default function Page() {
-  return <p>Trials Page</p>;
+  return <p>
+
+
+
+    <DialogTrigger>
+      <Button>Open dialog</Button>
+      <Modal isKeyboardDismissDisabled>
+        <Dialog>
+          <Heading slot="title">Notice</Heading>
+          <p>You must close this dialog using the button below.</p>
+          <Button slot="close">Close</Button>
+        </Dialog>
+      </Modal>
+    </DialogTrigger>
+  </p>;
 }
