9b2cf7f81c255923830f01c99f2fcb23888f5949
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-07-26 12:28:26 +1000

    assetstable search bar fix

--- app/ui/Assetstable.tsx
+++ app/ui/Assetstable.tsx
@@ -34,7 +34,8 @@ export default function AssetsTable({ assets , productTypes , supplierNmanufactu
   };
 
   const filteredAssets = assets.filter((asset) => {
-    const term = searchTerm.toLowerCase();
+    const term = debouncedSearch.toLowerCase(); // Use debounced input!
+
     return (
       (asset.asset_number || '').toLowerCase().includes(term) ||
       (asset.manufacturer_number || '').toLowerCase().includes(term) ||
@@ -48,13 +49,17 @@ export default function AssetsTable({ assets , productTypes , supplierNmanufactu
         ? new Date(asset.last_service_date).toISOString().toLowerCase()
         : ''
       ).includes(term) ||
-      (asset.product_type_id || '').toString().toLowerCase().includes(term) ||
-      (asset.asset_type_id || '').toString().toLowerCase().includes(term) ||
-      (asset.manufacturer_id || '').toString().toLowerCase().includes(term) ||
-      (asset.supplier_id || '').toString().toLowerCase().includes(term)
-
+      (asset.service_due_date
+        ? new Date(asset.service_due_date).toISOString().toLowerCase()
+        : ''
+      ).includes(term) ||
+      (productTypeMap.get(asset.product_type_id)?.toLowerCase() || '').includes(term) ||
+      (assetTypeMap.get(asset.asset_type_id || 0)?.toLowerCase() || '').includes(term) ||
+      (supplierMap.get(asset.manufacturer_id || 0)?.toLowerCase() || '').includes(term) ||
+      (supplierMap.get(asset.supplier_id || 0)?.toLowerCase() || '').includes(term)
     );
   });
+
 
 
   return (
