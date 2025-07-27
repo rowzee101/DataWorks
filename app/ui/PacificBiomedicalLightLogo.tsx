16ae123f91991123890f7635ea709729a417113a
rowzee101 <142756028+rowzee101@users.noreply.github.com>
2025-07-27 19:11:58 +1000

    cum

--- app/ui/PacificBiomedicalLightLogo.tsx
+++ app/ui/PacificBiomedicalLightLogo.tsx
@@ -3,9 +3,11 @@ import { lusitana } from '@/app/ui/fonts';
 
 type PACBioLogoProps = {
   className?: string;
+  width?: number;
+  height?: number;
 };
 
-export default function PACBioLogo({ className }: PACBioLogoProps) {
+export default function PACBioLogo({ className, width, height }: PACBioLogoProps) {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white ${className}`}
@@ -13,8 +15,8 @@ export default function PACBioLogo({ className }: PACBioLogoProps) {
       <Image
         src="/Logo-W-Text.png"
         alt="Pacific Bio-medical Logo"
-        width={204}
-        height={60}
+        width={width || 204}
+        height={height || 60}
       />
     </div>
   );
